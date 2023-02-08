8584571feef2903f9d7aea58a84ccb1cda637295
Omar Cerpa <cerpa_om@hotmail.com>
2023-02-08 02:45:19 -0600

    tercer commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import CartWidget from './CartWidget'
 const NavBar = () => {
   return (
     <>
-    <nav class="navbar bg-light">
+    <nav class="navbar navbar-dark bg-secondary">
   <div class="container">
     <a class="navbar-brand" href="#">
       <img src={logo} alt="Logo" width="100" height="50" class="d-inline-block align-text-top"/>
